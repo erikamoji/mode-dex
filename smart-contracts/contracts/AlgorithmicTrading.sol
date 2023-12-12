83015794f979ab82eb24a688a367fd1d4e32cd0a
erikamoji <ekpnguyen@gmail.com>
2023-12-12 20:14:05 +0700

    safemath, chainlink keeper

--- smart-contracts/contracts/AlgorithmicTrading.sol
+++ smart-contracts/contracts/AlgorithmicTrading.sol
@@ -2,26 +2,51 @@
 pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
-contract AlgorithmicTrading {
+contract AlgorithmicTrading is KeeperCompatibleInterface, Ownable {
+    using SafeMath for uint256;
+
     uint256[] public prices;  
     uint256 public smaPeriod = 5;
     uint256 public constant RSI_PERIOD = 14;
     uint256 public constant RSI_OVERBOUGHT = 70;
     uint256 public constant RSI_OVERSOLD = 30;
+    uint256 public lastUpdate;
+
+    // Keeper variables
+    uint256 public immutable interval;
+    uint256 public lastTimeStamp;
 
     AggregatorV3Interface internal priceFeed;
 
     event TradeExecuted(address indexed trader, bool buy, uint256 amount);
 
-    constructor(address _priceFeed) {
+    constructor(address _priceFeed, uint256 _updateInterval) {
         priceFeed = AggregatorV3Interface(_priceFeed);
+        interval = _updateInterval;
+        lastTimeStamp = block.timestamp;
+    }
+
+    function checkUpkeep(bytes calldata) external override returns (bool upkeepNeeded, bytes memory) {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+    }
+
+    function performUpkeep(bytes calldata) external override {
+        if ((block.timestamp - lastTimeStamp) > interval ) {
+            lastTimeStamp = block.timestamp;
+            updatePriceFromOracle();
+        }
     }
 
     // Function to update price from Chainlink Oracle
-    function updatePriceFromOracle() public {
-        (, int256 price, , , ) = priceFeed.latestRoundData();
+    function updatePriceFromOracle() internal {
+        (, int256 price, , uint256 timeStamp, ) = priceFeed.latestRoundData();
         require(price > 0, "Invalid price data");
+        require(timeStamp > lastUpdate, "No new data");
+        lastUpdate = timeStamp;
         prices.push(uint256(price));
     }
 
