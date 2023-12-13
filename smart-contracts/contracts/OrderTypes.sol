636f686628cd4457e1d99fae791408b5e3113447
erikamoji <ekpnguyen@gmail.com>
2023-12-13 18:55:14 +0700

    Added an interface IDex to define how OrderTypes interacts with the DEX, specifically for trade execution. The executeOrder function has been updated to call the DEX contract.

--- smart-contracts/contracts/OrderTypes.sol
+++ smart-contracts/contracts/OrderTypes.sol
@@ -4,6 +4,11 @@ pragma solidity ^0.8.0;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+interface IDex {
+    // Define interface for interacting with the DEX for executing orders.
+    function executeTrade(address trader, uint256 amount, uint256 price, bool isBuyOrder) external;
+}
+
 contract OrderTypes is Ownable {
     enum OrderType { Market, Limit, StopLoss }
     
@@ -17,19 +22,22 @@ contract OrderTypes is Ownable {
 
     Order[] public orders;
     AggregatorV3Interface public priceFeed;
+    IDex public dex;
 
     event OrderPlaced(uint256 indexed orderId, address indexed trader, OrderType orderType, uint256 amount, uint256 price);
     event OrderExecuted(uint256 indexed orderId, uint256 amount, uint256 executedPrice);
 
-    constructor(address _priceFeed) {
+    constructor(address _priceFeed, address _dex) {
         priceFeed = AggregatorV3Interface(_priceFeed);
+        dex = IDex(_dex);
     }
 
     function placeMarketOrder(uint256 amount) external {
         uint256 marketPrice = getLatestPrice();
         orders.push(Order(msg.sender, amount, marketPrice, OrderType.Market, true));
-        emit OrderPlaced(orders.length - 1, msg.sender, OrderType.Market, amount, marketPrice);
-        executeOrder(orders.length - 1);
+        uint256 orderId = orders.length - 1;
+        emit OrderPlaced(orderId, msg.sender, OrderType.Market, amount, marketPrice);
+        executeOrder(orderId, true); // Assume market orders are buy orders for simplicity
     }
 
     function placeLimitOrder(uint256 amount, uint256 limitPrice) external {
@@ -46,14 +54,15 @@ contract OrderTypes is Ownable {
         uint256 currentPrice = getLatestPrice();
         for (uint i = 0; i < orders.length; i++) {
             if (orders[i].isActive && isExecutable(orders[i], currentPrice)) {
-                executeOrder(i);
+                executeOrder(i, orders[i].orderType == OrderType.Limit);
             }
         }
     }
 
-    function executeOrder(uint256 orderId) internal {
-        // Logic for order execution: interacting with DEX, liquidity pools, etc.
-        // For simplicity, we'll just emit an event here.
+    function executeOrder(uint256 orderId, bool isBuyOrder) internal {
+        // Interact with DEX contract to execute the trade
+        dex.executeTrade(orders[orderId].trader, orders[orderId].amount, orders[orderId].price, isBuyOrder);
+
         emit OrderExecuted(orderId, orders[orderId].amount, orders[orderId].price);
         orders[orderId].isActive = false;
     }
@@ -72,11 +81,20 @@ contract OrderTypes is Ownable {
     function getLatestPrice() public view returns (uint256) {
         (
             , 
-            int price,
+            int256 price,
             ,
             ,
             
         ) = priceFeed.latestRoundData();
         return uint256(price);
+    }
+
+    // Additional functions to update price feed or DEX contract if needed
+    function updatePriceFeed(address _newPriceFeed) external onlyOwner {
+        priceFeed = AggregatorV3Interface(_newPriceFeed);
+    }
+
+    function updateDexContract(address _newDex) external onlyOwner {
+        dex = IDex(_newDex);
     }
 }
