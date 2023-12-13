624683935f86e7059c8d063621e2e1f4532386e6
erikamoji <ekpnguyen@gmail.com>
2023-12-13 19:16:13 +0700

    Gas Optimization, Access Control, Error Handling and Input Validation

--- smart-contracts/contracts/MyDex.sol
+++ smart-contracts/contracts/MyDex.sol
@@ -5,20 +5,39 @@ import "./MarginTrading.sol";
 import "./AlgorithmicTrading.sol";
 import "./OrderTypes.sol";
 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 interface SFSContract {
     function register(address _recipient) external returns (uint256);
 }
 
-contract MyDex {
+interface ILiquidityPool {
+    function getCurrentPrice(address token) external view returns (uint256);
+
+    function executeTrade(
+        address trader,
+        address token,
+        uint256 amount,
+        uint256 price,
+        bool isBuyOrder
+    ) external;
+}
+
+contract MyDex is Ownable {
     MarginTrading public marginTrading;
     AlgorithmicTrading public algorithmicTrading;
     OrderTypes public orderTypes;
     address public sfsContractAddress;
     uint256 public sfsTokenId;
+    ILiquidityPool public liquidityPool;
 
     event RegisteredWithSFS(address indexed dex, uint256 tokenId);
 
-    constructor(address _marginTradingAddress, address _algorithmicTradingAddress, address _orderTypesAddress) {
+    constructor(
+        address _marginTradingAddress,
+        address _algorithmicTradingAddress,
+        address _orderTypesAddress
+    ) {
         marginTrading = MarginTrading(_marginTradingAddress);
         algorithmicTrading = AlgorithmicTrading(_algorithmicTradingAddress);
         orderTypes = OrderTypes(_orderTypesAddress);
@@ -31,10 +50,45 @@ contract MyDex {
         emit RegisteredWithSFS(address(this), sfsTokenId);
     }
 
-    // Function to handle trade execution
-    function executeTrade(address trader, uint256 amount, uint256 price, bool isBuyOrder) external {
-        // Trade execution logic goes here
-        // This could involve interacting with liquidity pools and executing trades based on the provided parameters
+    // Set the liquidity pool (can be done in the constructor or via a separate setter function)
+    function setLiquidityPool(
+        address _liquidityPoolAddress
+    ) external onlyOwner {
+        liquidityPool = ILiquidityPool(_liquidityPoolAddress);
+    }
+
+    function executeTrade(
+        address trader,
+        address token,
+        uint256 amount,
+        uint256 price,
+        bool isBuyOrder
+    ) external {
+        require(token != address(0), "Invalid token address");
+        require(amount > 0, "Trade amount must be greater than zero");
+        require(
+            liquidityPool != ILiquidityPool(address(0)),
+            "Liquidity pool not set"
+        );
+
+        uint256 marketPrice = liquidityPool.getCurrentPrice(token);
+
+        if (isBuyOrder) {
+            require(marketPrice <= price, "Market price is too high");
+        } else {
+            require(marketPrice >= price, "Market price is too low");
+        }
+
+        // Execute the trade on the liquidity pool
+        liquidityPool.executeTrade(
+            trader,
+            token,
+            amount,
+            marketPrice,
+            isBuyOrder
+        );
+
+        emit TradeExecuted(trader, amount, marketPrice, isBuyOrder);
     }
 
     // Additional DEX functionalities can be added here
