1fbc3ed173635edfbbea22d68fd66b09d92a1b88
xj85770 <xavierdjones@gmail.com>
2023-12-13 19:49:13 +0700

    commit-reveal scheme

--- smart-contracts/contracts/OrderTypes.sol
+++ smart-contracts/contracts/OrderTypes.sol
@@ -3,13 +3,16 @@ pragma solidity ^0.8.0;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/keccak256.sol";
 
 interface IDex {
-    // Define interface for interacting with the DEX for executing orders.
     function executeTrade(address trader, uint256 amount, uint256 price, bool isBuyOrder) external;
 }
 
 contract OrderTypes is Ownable {
+    using ECDSA for bytes32;
+
     enum OrderType { Market, Limit, StopLoss }
     
     struct Order {
@@ -20,34 +23,51 @@ contract OrderTypes is Ownable {
         bool isActive;
     }
 
+    struct Commit {
+        bytes32 hash;
+        uint256 revealBlockNumber;
+        bool revealed;
+    }
+
+    uint256 public constant REVEAL_BLOCK_DELAY = 10;  // Number of blocks to wait before revealing
+    mapping(uint256 => Commit) public commits;
     Order[] public orders;
     AggregatorV3Interface public priceFeed;
     IDex public dex;
+    uint256 public nextCommitId;
 
-    event OrderPlaced(uint256 indexed orderId, address indexed trader, OrderType orderType, uint256 amount, uint256 price);
-    event OrderExecuted(uint256 indexed orderId, uint256 amount, uint256 executedPrice);
+    event CommitPlaced(uint256 indexed commitId, bytes32 hash, uint256 revealBlockNumber);
+    event OrderRevealed(uint256 indexed commitId, uint256 indexed orderId, address indexed trader, OrderType orderType, uint256 amount, uint256 price);
 
     constructor(address _priceFeed, address _dex) {
         priceFeed = AggregatorV3Interface(_priceFeed);
         dex = IDex(_dex);
     }
 
-    function placeMarketOrder(uint256 amount) external {
-        uint256 marketPrice = getLatestPrice();
-        orders.push(Order(msg.sender, amount, marketPrice, OrderType.Market, true));
-        uint256 orderId = orders.length - 1;
-        emit OrderPlaced(orderId, msg.sender, OrderType.Market, amount, marketPrice);
-        executeOrder(orderId, true); // Assume market orders are buy orders for simplicity
+    function placeOrderCommit(bytes32 hash) external {
+        uint256 commitId = nextCommitId++;
+        commits[commitId] = Commit({
+            hash: hash,
+            revealBlockNumber: block.number + REVEAL_BLOCK_DELAY,
+            revealed: false
+        });
+        emit CommitPlaced(commitId, hash, block.number + REVEAL_BLOCK_DELAY);
     }
 
-    function placeLimitOrder(uint256 amount, uint256 limitPrice) external {
-        orders.push(Order(msg.sender, amount, limitPrice, OrderType.Limit, true));
-        emit OrderPlaced(orders.length - 1, msg.sender, OrderType.Limit, amount, limitPrice);
-    }
+    function revealOrder(uint256 commitId, uint256 amount, uint256 price, OrderType orderType, bytes memory signature) external {
+        Commit storage commit = commits[commitId];
+        require(block.number >= commit.revealBlockNumber, "Reveal block number not yet reached");
+        require(!commit.revealed, "Order already revealed");
+        
+        bytes32 hash = keccak256(abi.encodePacked(msg.sender, amount, price, orderType));
+        require(hash.toEthSignedMessageHash().recover(signature) == msg.sender, "Invalid signature");
 
-    function placeStopLossOrder(uint256 amount, uint256 stopLossPrice) external {
-        orders.push(Order(msg.sender, amount, stopLossPrice, OrderType.StopLoss, true));
-        emit OrderPlaced(orders.length - 1, msg.sender, OrderType.StopLoss, amount, stopLossPrice);
+        require(hash == commit.hash, "Hash mismatch");
+        commit.revealed = true;
+        
+        uint256 orderId = orders.length;
+        orders.push(Order(msg.sender, amount, price, orderType, true));
+        emit OrderRevealed(commitId, orderId, msg.sender, orderType, amount, price);
     }
 
     function checkAndExecuteOrders() external onlyOwner {
